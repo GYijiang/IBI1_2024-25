e0996013da37891de28a2913bd2cfeed1ca64b5b
GYijiang <yijiang.24@intl.zju.edu.cn>
2025-03-11 11:09:03 +0800

    ii

---
+++ Pratical4/bmi_calculator.py
@@ -0,0 +1,10 @@
+a = float(input("kg"))
+b = float(input("m"))
+c = b**2
+d = a / c
+if d < 18.5 :
+    print("underweight")
+if d > 30 :
+    print("obese")
+else :
+    print("normal weight")
---
+++ Pratical4/triangle.py
@@ -0,0 +1,6 @@
+a = 1
+sum = 0
+for i in range(1,10):
+    sum = a + sum
+    a = a + 1
+    print(sum,end=" ")
