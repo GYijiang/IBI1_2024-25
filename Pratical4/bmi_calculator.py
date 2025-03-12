1a50c1380fbbb6e4c8eb962d50e369e20353842e
GYijiang <yijiang.24@intl.zju.edu.cn>
2025-03-12 14:07:20 +0800

    yep

--- Pratical4/bmi_calculator.py
+++ Pratical4/bmi_calculator.py
@@ -1,10 +1,14 @@
-a = float(input("  kg"))
-b = float(input("  m"))
+a = float(input("kg"))
+b = float(input("m"))
 c = b**2
 d = a / c
+#The value of "d" is calculated by dividing the value of "a" by the square of the value of "b".
 if d < 18.5 :
     print("underweight")
+#The value of "d" is less than 18.5, so the string "underweight" is printed.
 if d > 30 :
     print("obese")
+#The value of "d" is greater than 30, so the string "obese" is printed.
 else :
     print("normal weight")
+#The value of "d" is not less than 18.5 and not greater than 30, so the string "normal weight" is printed.
