6d0488a0f19884d29f8b6e9e00e97d8c6ecd94b8
GYijiang <yijiang.24@intl.zju.edu.cn>
2025-04-27 15:38:57 +0800

    Update bar.py

--- Practical 5/bar.py
+++ Practical 5/bar.py
@@ -1,27 +1,27 @@
-language = {'JavaScript' : 62.3, 'HTML' : 52.9, 'Python' : 51, 'SQL' : 51, 'TypeScript' : 38.5 } # dictionary
-print(language) # print the dictionary
-key = input("Enter the key to check: ") # input from the user
-# check if the key is present in the dictionary
-if key in language: 
-    print("Key is present in the dictionary",'=', language[key])
-# if the key is not present in the dictionary
-# print the message
+language = {'JavaScript': 62.3, 'HTML': 52.9, 'Python': 51, 'SQL': 51, 'TypeScript': 38.5}
+print(language)  # print the dictionary
+
+key = input("Enter the key to check: ")  # input from the user
+if key in language:
+    print("Key is present in the dictionary", '=', language[key])
 else:
-    print("Key is not present in the dictionary",'=', key)
-    
+    print("Key is not present in the dictionary", '=', key)
 
 import numpy as np
-import matplotlib.pyplot as plt # import the library
-N = 5 # number of languages
-scores = (62.3,52.9,51,51,38.5) # scores of the languages
-# create an array of the languages
-ind = np.arange(N)
-# create the bar chart
+import matplotlib.pyplot as plt
+
+# Extract data directly from the dictionary
+languages = list(language.keys())  # ['JavaScript', 'HTML', 'Python', 'SQL', 'TypeScript']
+scores = list(language.values())  # [62.3, 52.9, 51, 51, 38.5]
+
+N = len(language)  # number of languages (5)
+ind = np.arange(N)  # [0, 1, 2, 3, 4]
+
 width = 0.35
-p1 = plt.bar(ind, scores, width)
-# add some text for labels, title and axes ticks
-plt.ylabel('Scores')# y-axis label
-plt.title('Scores by language')# title of the chart
-plt.xticks(ind, ('JavaScript', 'HTML', 'Python', 'SQL', 'TypeScript'))# x-axis labels
-plt.yticks(np.arange(0, 81, 10))# y-axis ticks
-plt.show()# display the chart
+plt.bar(ind, scores, width)
+
+plt.ylabel('Scores')
+plt.title('Scores by language')
+plt.xticks(ind, languages)  # Use dictionary keys as x-axis labels
+plt.yticks(np.arange(0, 81, 10))  # y-axis from 0 to 80, step=10
+plt.show()
